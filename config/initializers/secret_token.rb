d7304b424f9aef46b0fa61a2261d7f49d54f3595
kobake <kobake@users.sourceforge.net>
2015-01-19 23:22:34 +0900

    rails new x-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XRails::Application.config.secret_key_base = '9238c8c047bbf7ea8d60b9e64b13bbc10a74784dbe48237805b1864442e20703ca93964d3b42a6aff0bb3bf78eea5788119577303c630dd4fcef3a7bbc9bf735'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XRails::Application.config.session_store :cookie_store, key: '_x-rails_session'
